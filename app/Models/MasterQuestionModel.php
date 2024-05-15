2c9b98a32b7c2f5772280a2e42cdf1e1f09dd1c5
nanangfajaru <nanangfajaru@gmail.com>
2024-05-15 13:51:23 +0700

    Auth Question Model

--- app/Models/MasterQuestionModel.php
+++ app/Models/MasterQuestionModel.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
+use Auth;
 use App\Models\OrganizationModels;
 
 class MasterQuestionModel extends Model
